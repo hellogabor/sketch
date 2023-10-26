53f0673a7a7420618287d3311cdea4e214c8a427
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-26 21:52:05 +0200

    add grid adjust feature

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const gridCont = document.querySelector('#grid-container');
 
-for (let i = 0; i < 16; i++) {
+let numberOfBoxes = 16;
+
+let createGrid = function() { for (let i = 0; i < numberOfBoxes; i++) {
     let gridRow = document.createElement('div');
     gridRow.className = 'gridRow';
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < numberOfBoxes; i++) {
         let gridElem = document.createElement('div');
         gridElem.style.border = '0.1px solid black';
         gridElem.style.width = '10px';
@@ -16,4 +18,14 @@ for (let i = 0; i < 16; i++) {
     gridRow.style.display = 'flex';
     gridCont.appendChild(gridRow);
 }
+}
+
+createGrid();
+
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', function() {
+    gridCont.innerHTML = "";
+    numberOfBoxes = document.querySelector('#input').value;
+    createGrid();
+});
 
