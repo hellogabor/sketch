d78e7d62fec62308b7b75cefd734c87cabc37c26
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-26 20:35:03 +0200

    create grid

---
+++ script.js
@@ -0,0 +1,16 @@
+const gridCont = document.querySelector('#grid-container');
+
+for (let i = 0; i < 16; i++) {
+    let gridRow = document.createElement('div');
+    gridRow.className = 'gridRow';
+    for (let i = 0; i < 16; i++) {
+        let gridElem = document.createElement('div');
+        gridElem.style.border = '0.1px solid black';
+        gridElem.style.width = '10px';
+        gridElem.style.height = '10px';
+        gridRow.appendChild(gridElem);
+    }
+    gridRow.style.display = 'flex';
+    gridCont.appendChild(gridRow);
+}
+
