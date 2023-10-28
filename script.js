d97eb25ee7b9daf064bbfbfe7f5008d10a322673
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-28 20:51:46 +0200

    add random color toggle

--- script.js
+++ script.js
@@ -2,49 +2,9 @@ const gridCont = document.querySelector('#grid-container');
 
 let numberOfBoxes = 16;
 
-// const colorChanger = function(item) {
-//     item.style.backgroundColor = 'black';
-//     item.style.opacity="0.1";
-// }
+let colorChanger = darkener;
 
-// const colorChanger = function(item) {
-//     let opacityValue = parseFloat(item.getAttribute('data-opacity'));
-//     // let opacity = item.style.opacity=`${opacityValue}`;
-//     for (let i = 0; i <= 1; i+=0.1) {
-//         opacityValue += i;
-//         item.setAttribute('data-opacity', opacityValue);
-//         item.style.opacity=`${opacityValue}`;
-//     }
-// }
-
-// let createGrid = function() { for (let i = 0; i < numberOfBoxes; i++) {
-//     const gridRow = document.createElement('div');
-//     gridRow.className = 'gridRow';
-//     for (let i = 0; i < numberOfBoxes; i++) {
-//         const gridElem = document.createElement('div');
-//         gridElem.style.border = '0.1px solid black';
-//         gridElem.style.width = 'auto';
-//         gridElem.style.height = 'auto';
-//         gridElem.style.flexGrow = '1';
-//         gridElem.style.display = 'flex';
-//         gridRow.appendChild(gridElem);
-//         const gridInnerElem = document.createElement('div');
-//         gridInnerElem.style.flexGrow = '1';
-//         gridInnerElem.style.backgroundColor = 'black';
-//         gridInnerElem.style.opacity='0';
-//         gridInnerElem.setAttribute('data-opacity', '0');
-//         gridElem.appendChild(gridInnerElem);
-//         gridInnerElem.addEventListener('mouseover', () => colorChanger(gridInnerElem));
-//     }
-//     gridRow.style.display = 'flex';
-//     gridRow.style.flexGrow = '1';
-//     gridCont.appendChild(gridRow);
-// }
-// }
-
-// createGrid();
-
-const colorChanger = function(item) {
+function darkener(item) {
     let opacityValue = parseFloat(item.getAttribute('data-opacity'));
     opacityValue += 0.2;
     if (opacityValue > 1) {
@@ -52,6 +12,14 @@ const colorChanger = function(item) {
     }
     item.setAttribute('data-opacity', opacityValue);
     item.style.opacity = `${opacityValue}`;
+}
+
+const randomColor = () => "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+function colorizer(item) {
+    item.style.backgroundColor = `${randomColor()}`;
+    item.style.opacity = '1';
+    // console.log(item.style.backgroundColor = `${randomColor}`);
 }
 
 let createGrid = function() { 
@@ -99,6 +67,20 @@ btn.addEventListener('click', function() {
 const resetbtn = document.querySelector('#resetbtn');
 
 resetbtn.addEventListener('click', function() {
+    numberOfBoxes = 16;
+    gridCont.innerHTML = "";
+    createGrid();
+});
+
+const toggleRandom = document.querySelector('#randomcolors');
+
+toggleRandom.addEventListener('click', function() {
+    if(colorChanger == darkener) {
+        colorChanger = colorizer;
+    } else {
+        colorChanger = darkener;
+    }
+    numberOfBoxes = 16;
     gridCont.innerHTML = "";
     createGrid();
 });
