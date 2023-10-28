56bf0017f205b1271219cc63838bba690a2abbfa
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-28 17:00:41 +0200

    set fixed grid dimensions

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ const gridCont = document.querySelector('#grid-container');
 
 let numberOfBoxes = 16;
 
+
+
 let createGrid = function() { for (let i = 0; i < numberOfBoxes; i++) {
     let gridRow = document.createElement('div');
     gridRow.className = 'gridRow';
     for (let i = 0; i < numberOfBoxes; i++) {
         let gridElem = document.createElement('div');
         gridElem.style.border = '0.1px solid black';
-        gridElem.style.width = '10px';
-        gridElem.style.height = '10px';
+        gridElem.style.width = 'auto';
+        gridElem.style.height = 'auto';
+        gridElem.style.flexGrow = '1';
         gridRow.appendChild(gridElem);
         gridElem.addEventListener('mouseover', function() {
             this.style.backgroundColor = 'black';
         });
     }
     gridRow.style.display = 'flex';
+    gridRow.style.flexGrow = '1';
     gridCont.appendChild(gridRow);
 }
 }
@@ -26,6 +30,9 @@ let btn = document.querySelector('#btn');
 btn.addEventListener('click', function() {
     gridCont.innerHTML = "";
     numberOfBoxes = document.querySelector('#input').value;
+    if (numberOfBoxes > 100) {
+        numberOfBoxes = 100;
+    }
     createGrid();
 });
 
